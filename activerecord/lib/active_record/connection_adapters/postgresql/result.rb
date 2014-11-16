1b4eaccda0960d7da691714b1a842fd298361de9
Lars Kanis <lars@greiz-reinsdorf.de>
2014-11-16 20:59:00 +0100

    cleanup

--- activerecord/lib/active_record/connection_adapters/postgresql/result.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql/result.rb
@@ -6,22 +6,15 @@ module ActiveRecord
         attr_writer :column_types
         attr_writer :columns
 
-        def initialize(conn, on_error, stream_threshold=1000)
+        def initialize(conn, on_error)
           @connection   = conn
           @on_error     = on_error
-          @stream_threshold = stream_threshold
           @pgresult     = nil
           @columns      = nil
           @rows         = nil
           @hash_rows    = nil
           @column_types = nil
         end
-
-#         def map(&block)
-#           @pgresult.stream_each do |row|
-#
-#           end
-#         end
 
         def initialize_copy(other)
           @pgresult     = pgresult
@@ -53,56 +46,50 @@ module ActiveRecord
         end
 
         def finish_streaming
-          rows if @pgresult
+          if @pgresult
+            rows
+            # Clear result queue
+            @connection.get_last_result
+          end
         end
 
         def rows
           @rows ||= begin
             if @pgresult.result_status == ::PG::PGRES_SINGLE_TUPLE
-              rows = @pgresult.stream_each_row.to_a
-              # Clear result queue
-              @connection.get_last_result
-#           puts rows.inspect
-
-              rows
+              @pgresult.stream_each_row.to_a
             else
-              @connection.get_last_result
               []
             end
           rescue => e
             @on_error.call(e)
           end
-          @rows
         end
 
         private
 
         def hash_rows
           @hash_rows ||= begin
-            rows.map { |row| row_to_hash_row( row ) }
+            # The field_name strings are frozen when retrieved from the PG::Result object.
+            # This prevents them getting duped when used as keys in ActiveRecord::Base's @attributes hash
+            columns = @columns
+            length = columns.length
+
+            rows.map do |row|
+              # In the past we used Hash[columns.zip(row)]
+              #  though elegant, the verbose way is much more efficient
+              #  both time and memory wise cause it avoids a big array allocation
+              #  this method is called a lot and needs to be micro optimised
+              hash = {}
+
+              index = 0
+
+              while index < length
+                hash[columns[index]] = row[index]
+                index += 1
+              end
+              hash
+            end
           end
-        end
-
-        def row_to_hash_row(row)
-          # In the past we used Hash[columns.zip(row)]
-          #  though elegant, the verbose way is much more efficient
-          #  both time and memory wise cause it avoids a big array allocation
-          #  this method is called a lot and needs to be micro optimised
-          hash = {}
-
-          index = 0
-
-          # The field_name strings are frozen when retrieved from the PG::Result object.
-          # This prevents them getting duped when used as keys in ActiveRecord::Base's @attributes hash
-          columns = @columns
-          length = columns.length
-
-          while index < length
-            hash[columns[index]] = row[index]
-            index += 1
-          end
-
-          hash
         end
       end
     end
