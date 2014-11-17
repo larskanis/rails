2a12a360b2ae0fad2f0a346a995d3154efa32918
Lars Kanis <kanis@comcard.de>
2014-11-17 14:38:07 +0100

    PostgreSQL, Make use of new type cast encoders and decoders provided by latest ruby-pg gem for integer, float, bytea, boolean and array types.
    It extends the statement pool, so that it stores several fixed parameters
    in addition to the prepared statement key of the cached query.
    
    This currently breaks compatiblity with the java port of ruby-pg, but it
    will hopefully follow soon.

--- activerecord/lib/active_record/connection_adapters/postgresql/oid/float.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql/oid/float.rb
@@ -5,14 +5,13 @@ module ActiveRecord
         class Float < Type::Float # :nodoc:
           include Infinity
 
-          def cast_value(value)
-            case value
-            when ::Float then     value
-            when 'Infinity' then  ::Float::INFINITY
-            when '-Infinity' then -::Float::INFINITY
-            when 'NaN' then       ::Float::NAN
-            else                  value.to_f
-            end
+          attr_reader :pg_encoder
+          attr_reader :pg_decoder
+
+          def initialize(options = {})
+            super
+            @pg_encoder = PG::TextEncoder::Float.new name: type
+            @pg_decoder = PG::TextDecoder::Float.new name: type
           end
         end
       end
